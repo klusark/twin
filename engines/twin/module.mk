ff314b93217e1cf1c113777a5916f60f45790de0
xesf <xesfnet@gmail.com>
2013-07-16 01:15:19 +0100

    TWIN: update module.mk including video_player files

--- engines/twin/module.mk
+++ engines/twin/module.mk
@@ -7,7 +7,10 @@ MODULE_OBJS := \
 	image.o \
 	lzss.o \
 	scene.o \
-	twin.o
+	twin.o \
+	video_player.o \
+	video_player_fla.o \
+	video_player_smk.o 
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_TWIN), DYNAMIC_PLUGIN)
