42bf96d2b75dddced79784c2e0fbdcd3c775f6ae
Joel Teichroeb <joel@teichroeb.net>
2013-08-04 22:52:59 -0700

    TWIN: Load more of the actor data correctly

--- engines/twin/actor.cpp
+++ engines/twin/actor.cpp
@@ -41,12 +41,13 @@ Actor::Actor(Common::SeekableReadStream *stream) : _entity(nullptr) {
 void Actor::loadLBA2(Common::SeekableReadStream *stream) {
 	//These sizes are probably mostly wrong, but the overall actor size is right
 	uint16 flags = stream->readUint16LE();
+	uint16 flag2 = stream->readUint16LE();
+
+	_entityID = stream->readUint16LE();
 	_body = stream->readByte();
 	stream->readByte();
-	_entityID = stream->readUint16LE();
 	_anim = stream->readByte();
 	_sprite = stream->readUint16LE();
-	stream->readUint16LE();
 	_x = stream->readUint16LE();
 	_y = stream->readUint16LE();
 	_z = stream->readUint16LE();
@@ -67,6 +68,15 @@ void Actor::loadLBA2(Common::SeekableReadStream *stream) {
 	stream->readByte();
 	stream->readUint16LE();
 	stream->readByte();
+
+	if (flag2 & 0x004) {
+		stream->readByte();
+		stream->readByte();
+		stream->readByte();
+		stream->readByte();
+		stream->readByte();
+		stream->readByte();
+	}
 
 	_moveScriptSize = stream->readUint16LE();
 	_moveScript = new uint8[_moveScriptSize];
